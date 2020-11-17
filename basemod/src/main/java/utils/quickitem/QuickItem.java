50903326ca3da6305e27ee749d525be2af1043ec
Joseph Collard <jcollard@chadwickschool.org>
2020-11-17 09:58:46 -0800

    Testing a thing

--- basemod/src/main/java/utils/quickitem/QuickItem.java
+++ basemod/src/main/java/utils/quickitem/QuickItem.java
@@ -146,6 +146,7 @@ public class QuickItem {
 		for (char c : name.toCharArray()) {
 			if (Character.isLetter(c) || Character.isDigit(c)) {
 				newName.append(c);
+				System.out.println("Here!");
 			} else {
 				newName.append("_");
 			}
