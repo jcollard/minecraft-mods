46eef29c863a2f84b6b8323541cc7b688c183ffb
Local User <local@user.com>
2020-12-17 10:20:54 -0800

    Created a Food.Builder and made it restore hunger.

--- basemod/src/main/java/items/Banana.java
+++ basemod/src/main/java/items/Banana.java
@@ -1,9 +1,8 @@
 package items;
 
 import net.minecraft.item.Food;
+import net.minecraft.item.Foods;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
 import net.minecraft.item.Item.Properties;
 import utils.quickitem.QuickItem;
 
@@ -21,24 +20,12 @@ public class Banana extends QuickItem {
 		// Specify which tab this item will appear in
 		properties.group(ItemGroup.FOOD);
 		
-		// Instantiate a Food.Builder
+		// Creates a new Food.Builder to build new food
 		Food.Builder foodBuilder = new Food.Builder();
-		// Specify how much saturation this food provides
-		foodBuilder.saturation(0.3F);
-		// Specify how much hunger this food provides
-		foodBuilder.hunger(4);
-		// Create a resistance EffectInstance
-		EffectInstance resistance = new EffectInstance(Effects.RESISTANCE, 1600);
-		// Add the resistance EffectInstance to our food and make it happen 100% of the time
-		foodBuilder.effect(resistance, 1.0F);
-		// Create a regeneration EffectInstance
-		EffectInstance regeneration = new EffectInstance(Effects.REGENERATION, 400);
-		// Add the regeneration effect to our food and make it happen 50% of the time
-		foodBuilder.effect(regeneration, 0.5F);
+		foodBuilder.hunger(2);
+		
 		// Specify the effects of eating this item
 		properties.food(foodBuilder.build());
 	}
-	
-	
 	
 }
