de862d603837df9e66c82745e1d588f4d6ecbd5e
Local User <local@user.com>
2021-05-14 09:06:39 -0700

    Making a backup

---
+++ basemod/src/main/java/blocks/RubyBlock.java
@@ -0,0 +1,19 @@
+package blocks;
+
+import net.minecraft.block.Block.Properties;
+import utils.quickblock.QuickBlock;
+
+public class RubyBlock extends QuickBlock {
+
+	{
+		blockName = "Block of Ruby";
+		texture = "ruby";
+	}
+
+	@Override
+	public void initializeProperties(Properties p) {
+		// TODO Auto-generated method stub
+		super.initializeProperties(p);
+	}
+
+}
